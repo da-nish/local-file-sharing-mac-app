02f14f0199424d7fa9693bbf1b7798444b68da51
danish khan <danishdev64@gmail.com>
2025-12-09 11:18:45 +0530

    Initial Commit

---
+++ Mac_Local_File/Mac_Local_FileApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mac_Local_FileApp.swift
+//  Mac_Local_File
+//
+//  Created by PropertyShare on 09/12/25.
+//
+
+import SwiftUI
+
+@main
+struct Mac_Local_FileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
